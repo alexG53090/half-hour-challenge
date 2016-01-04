629554d9fb449588ae8f20e04fbc357ba2b60d1f
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-04 10:59:10 -0700

    add views :tv:

--- views/client.js
+++ views/client.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   getBroncosData().then(function(data){
-    printBroncos(data).then(function(data){
-      console.log(data)
+    formatBroncos(data).then(function(broncos){
+      lineBroncos(broncos).then(function(players){
+        printBroncos(players).then(function(players){
+          console.log(players)
+        })
+      })
     })
   })
 })
@@ -15,10 +19,39 @@ function getBroncosData(){
   })
 }
 
-function printBroncos(data){
-  for(i = 0; i < data.bcos.length; i = i +1){
-    var bronco = data.bcos[i]
-    console.log(bronco)
-    var listItem = document.createElement('p')
-  }
+function formatBroncos(data){
+  return new Promise(function(resolve, reject){
+    var broncos =[];
+    for(i = 0; i < data.bcos.length; i = i +1){
+      var bronco = data.bcos[i]
+      broncos.push(bronco)
+    }
+    resolve(broncos)
+  })
 }
+
+
+function lineBroncos(broncos){
+  var container = document.getElementById('broncosContainer')
+  return new Promise(function(resolve, reject){
+    var players = []
+    broncos.forEach(function(item, index, array){
+      var player = item;
+      players.push(player);
+      console.log(player)
+      var listItem = document.createElement('li')
+      var content = document.createTextNode(player)
+    })
+    resolve(players)
+  })
+}
+
+function printBroncos(players){
+  listItem.appendChild(player)
+  container.appendChild(listItem);
+}
+
+//
+//
+// listItem.appendChild(player)
+// container.appendChild(listItem);
